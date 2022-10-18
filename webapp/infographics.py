35a79094b023cc3661fd5a0fbedab3be237103be
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2022-10-18 23:50:25 +0100

    st.write test

--- webapp/infographics.py
+++ webapp/infographics.py
@@ -11,5 +11,10 @@ Yes... But i need to test if the website works
 and who in their right mind is going to see the 
 innital commit of this project""")
 
-
+st.write("""
+         also i have 8 versions of python and
+         none of them want to play nice
+         so i am havinng to write all the code in github
+         and this is less than idea for me
+         as there is really no way for me to test things""")
     
