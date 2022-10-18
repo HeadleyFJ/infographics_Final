d1996e5cdea919c110ac24a1af9ed22a7a3a68d3
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2022-10-19 00:11:22 +0100

    Update Infographic 1🐳.py

--- webapp/pages/Infographic 1🐳.py
+++ webapp/pages/Infographic 1🐳.py
@@ -3,3 +3,19 @@ import streamlit as st
 st.title("this is a different page")
 
 st.header("this is a different header")
+
+st.header("this is a button")
+
+if st.button('This is a button'):
+    st.write('Pressed')
+else:
+    st.write('Not Pressed')
+   
+  
+ st.header("this is a checkbox")
+  
+checkbox = st.checkbox('check this box')
+
+if agree:
+    st.write('you did it!')
+    
