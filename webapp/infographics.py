cd88e083442a8502cc8369dea1c33cbfe0547001
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2022-10-18 21:24:18 +0100

    create initial test file

--- webapp/infographics.py
+++ webapp/infographics.py
@@ -1,1 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+%matplotlib inline
 
+
+time = [0,1,2,3,4,5,6,7,8,9,10]
+cupcakes = [0,1,1,1,2,4,6,6,8,11]
+
+plt.scatter(time,cupcakes,c="black",marker='D',s=50)
+plt.xlabel('Time in days')
+plt.ylabel('Number of cupcakes I have')
+
+plt.show()
