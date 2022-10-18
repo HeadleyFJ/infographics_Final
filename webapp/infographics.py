06c44c7e876963d641c4c42c74c89d5d39dd0a7c
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2022-10-18 21:59:10 +0100

    Update infographics.py

--- webapp/infographics.py
+++ webapp/infographics.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 
-
+fig = plt.figure() 
 
 time = [0,1,2,3,4,5,6,7,8,9,10]
 cupcakes = [0,1,1,1,2,4,6,6,8,11,12]
@@ -18,7 +18,7 @@ plt.scatter(time,cupcakes,c="black",marker='D',s=50)
 plt.xlabel('Time in days')
 plt.ylabel('Number of cupcakes I have')
 
-plt.show()
+st.pyplot(fig)
 
 with st.expander("This talks more about cupcakes."):
     st.write("""
