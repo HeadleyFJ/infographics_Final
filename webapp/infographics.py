4dcb09743a9f2e9616630e7893e99648d1233a27
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2022-10-18 21:27:06 +0100

    correct code in initial test

--- webapp/infographics.py
+++ webapp/infographics.py
@@ -4,8 +4,8 @@ import scipy as sp
 %matplotlib inline
 
 
-time = [0,1,2,3,4,5,6,7,8,9,10,8]
-cupcakes = [0,1,1,1,2,4,6,6,8,11]
+time = [0,1,2,3,4,5,6,7,8,9,10]
+cupcakes = [0,1,1,1,2,4,6,6,8,11,12]
 
 plt.scatter(time,cupcakes,c="black",marker='D',s=50)
 plt.xlabel('Time in days')
