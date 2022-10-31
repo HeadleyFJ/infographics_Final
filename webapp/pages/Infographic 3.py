49b58b6272b34c0da62ae120d4a210531fd95548
BFry13 <116182256+BFry13@users.noreply.github.com>
2022-10-31 13:26:31 +0000

    Update Infographic 3.py

--- webapp/pages/Infographic 3.py
+++ webapp/pages/Infographic 3.py
@@ -63,7 +63,7 @@ bucket_location = 0.5*(s_Earth_horizontal[index]+s_Earth_horizontal[index+1])
 # print(bucket_location)
 
 fig = plt.figure(figsize=(20,8))
-polar_ax = plt.subplot(1, 2, 1, projection='polar')
+polar_ax = plt.subplot(2, 1, 1, projection='polar')
 polar_ax.set_theta_zero_location('N')
 polar_ax.set_theta_direction(-1)
 polar_ax.set_ylim((0*s_initial_vertical), (1.1*planet_radius[0]))
@@ -82,7 +82,7 @@ plt.ylabel("Radius $ \dfrac{r_{planet}}{r_{Earth}} $")
 polar_ax.axes.get_xaxis().set_visible(False)
 polar_ax.legend(loc=0)
 
-cartesian_ax = plt.subplot(1, 2, 2)
+cartesian_ax = plt.subplot(2, 1, 2)
 
 cartesian_ax.plot(s_planet_horizontal, s_planet_vertical, color='grey', ls='-', zorder=1)
 cartesian_ax.plot(s_Earth_horizontal, s_Earth_vertical, color='blue', alpha=0.5, ls='--', zorder=2)
