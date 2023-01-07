7499e58fcfc922bcf92410bd722b256b8ac137b4
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2023-01-06 22:08:16 -0500

    Update infographics.py

--- webapp/infographics.py
+++ webapp/infographics.py
@@ -22,16 +22,17 @@ st.header("Fin Headley's Infographic Project")
 
 
 st.write("""
-\n\n\n\n\n\n\n\n\n\n\n\n
+\n
 From the sidebar select one of two infographics. 
-\n\n\n\n\n\n\n\n
+\n
 Satellites will display the location of any satellites at any time chosen by the user.
-\n\n\n\n\n\n\n\n
+\n
 Moons will display all the orbiting moons of any plannet in the solar system.
-\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n""")
+\n""")
 
 
-
+st.markdown("***")
+st.markdown("***")
 
 with st.expander("Acknowledgments"):
     st.write("""
