fa19cd111d32de9342d1cf667d3b58a04f846a08
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2023-01-06 22:04:00 -0500

    Update infographics.py

--- webapp/infographics.py
+++ webapp/infographics.py
@@ -8,24 +8,30 @@ st.set_page_config(
 )
 
 
-st.write("[![Star](<https://img.shields.io/github/stars/><HeadleyFJ>/<infographics_Final>.svg?logo=github&style=social)](<https://gitHub.com/><HeadleyFJ>/<infographics_Final>)")
 
-st.sidebar.success("Select an Infographic or the intro page.")
+st.sidebar.success("Select an Infographic or the Info page.")
 
-st.header("Fin Headley's infographic Project")
+st.header("Fin Headley's Infographic Project")
 
+#with st.expander("This talks more about cupcakes."):
+#    st.write("""
+#Is this an unprofessional way of writing this out?
+#Yes... But i need to test if the website works
+#and who in their right mind is going to see the 
+#innital commit of this project""")
 
-with st.expander("This talks more about cupcakes."):
-    st.write("""
-Is this an unprofessional way of writing this out?
-Yes... But i need to test if the website works
-and who in their right mind is going to see the 
-innital commit of this project""")
 
 st.write("""
-         also i have 8 versions of python and
-         none of them want to play nice
-         so i am havinng to write all the code in github
-         and this is less than idea for me
-         as there is really no way for me to test things""")
+In the sidebar there are options for two info graphics. 
+\n
+Satellites will display the location of any satellites at any time chosen by the user.
+\n
+Moons will display all the orbiting moons of any plannet in the solar system.""")
+
+
+
+
+with st.expander("Acknowledgments"):
+    st.write("""
+         I would like to thank my entire group, as well as my superviser Chris North, who were an integeral part of this project.""")
     
