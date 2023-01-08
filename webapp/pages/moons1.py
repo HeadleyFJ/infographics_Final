1030a8a9e56bfbd7dd1b16e7c9dc7dc41ef6a423
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2023-01-08 01:51:25 -0500

    Update moons1.py

--- webapp/pages/moons1.py
+++ webapp/pages/moons1.py
@@ -34,7 +34,7 @@ if option =='jupiter':
 
 if option =='saturn':
     sim.add("699", hash = "Saturn")
-    for i in range(601,630):
+    for i in range(601,667):
         sim.add(str(i))
 
 if option =='uranus':
